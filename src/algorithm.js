0f5fb32d186522ac7835e0c288f6c635f3ff8a82
Theecypher <obioramma12@gmail.com>
2026-06-22 10:04:37 +0100

    Resolved conflicts

--- src/algorithm.js
+++ src/algorithm.js
@@ -11,52 +11,9 @@ console.log(pow(2, 2));
 console.log(pow(2, 3));
 console.log(pow(2, 4));
 
-function capitalize(letter) {
-  let splitValue = letter.split("");
 
-  let value = splitValue[0].toUpperCase();
-  let format = letter.split("").with(0, value).join("");
 
-  return format;
-}
 
-function reverseString(letter) {
-  let addedLength = letter.length;
-  let arr = [];
-
-  let str;
-
-  for (let i = addedLength; i >= 0; i--) {
-    if (i === -1) {
-      break;
-    }
-
-    arr.push(letter[i]);
-
-    arr.join(",");
-
-    str = arr.join("");
-  }
-  console.log(str);
-}
-
-const Calculator = {
-  add: function (a, b) {
-    return a + b;
-  },
-
-  subtract: function (a, b) {
-    return a - b;
-  },
-
-  divide: function (a, b) {
-    return a / b;
-  },
-
-  multiply: function (a, b) {
-    return a * b;
-  },
-};
 
 // console.log(Calculator.add(5, 4));
 // console.log(Calculator.divide(5, 4));
---
+++ src/reverseString.js
@@ -0,0 +1,19 @@
+function reverseString(letter) {
+  let addedLength = letter.length;
+  let arr = [];
+
+  let str;
+
+  for (let i = addedLength; i >= 0; i--) {
+    if (i === -1) {
+      break;
+    }
+
+    arr.push(letter[i]);
+
+    arr.join(",");
+
+    str = arr.join("");
+  }
+  return str
+}
